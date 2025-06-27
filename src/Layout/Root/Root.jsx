57c35030520b631376680f7422f83bdb953e191e
Al Ifran <ifran15-2915@diu.edu.bd>
2025-06-27 21:05:31 +0600

    navbar done

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../../Components/Header/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
 
 const Root = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
